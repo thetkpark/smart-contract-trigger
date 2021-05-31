2129e4e394d31d8e45c156001ec55ab49e811889
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-05-31 20:16:28 +0700

    FIX pickingWinner method

--- src/cscms-lottery.js
+++ src/cscms-lottery.js
@@ -1,25 +1,29 @@
-const Web3 = require('web3')
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-const interface = require('./abis/cscms-lottery.json')
-const dayjs = require('dayjs')
-require('dotenv').config()
+const Web3 = require("web3")
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const interface = require("./abis/cscms-lottery.json")
+const dayjs = require("dayjs")
+require("dotenv").config()
 
 const trigger = async () => {
 	const provider = new HDWalletProvider({
 		mnemonic: process.env.CSCMS_LOTTERY_MNEMONIC,
-		providerOrUrl: process.env.CSCMS_LOTTERY_PROVIDER_ENDPOINT,
+		providerOrUrl: process.env.CSCMS_LOTTERY_PROVIDER_ENDPOINT
 	})
 	const web3 = new Web3(provider)
 	const accounts = await web3.eth.getAccounts()
-	const lottery = new web3.eth.Contract(interface, process.env.CSCMS_LOTTERY_ADDRESS)
+	const lottery = new web3.eth.Contract(
+		interface,
+		process.env.CSCMS_LOTTERY_ADDRESS
+	)
 	const endTime = await lottery.methods.endTime().call()
 	const isBeforeEndtime = dayjs().isBefore(dayjs.unix(endTime))
-	if (isBeforeEndtime) return
-	await lottery.methods.pickWinner.send({
+	if (isBeforeEndtime) throw new Error("Not the ending time yet")
+	await lottery.methods.pickWinner().send({
 		from: accounts[0],
+		gas: "1000000"
 	})
 }
 
 trigger()
-	.then(() => console.log('DONE triggering pick winner'))
-	.catch(err => console.log(JSON.stringify(err)))
+	.then(() => console.log("DONE triggering pick winner"))
+	.catch((err) => console.log(err))
